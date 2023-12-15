8e27c30df486d1ec0c79881fbfccda03f41eaf2d
Magnus Taylor <magnusjtaylor21@gmail.com>
2023-12-15 17:54:52 -0500

    Starting to implement Route Logic

--- Plugin.cs
+++ Plugin.cs
@@ -70,26 +70,43 @@ namespace RouteManager
         }
     }
 
-    [HarmonyPatch(typeof(TrainController))]
     public static class TrainControllerPatch
     {
-        [HarmonyPostfix, HarmonyPatch("GetCenterPointOfCar")]
-        public static void GetCenterPointOfCarPostfix(TrainController __instance, string carId, ref Vector3? __result)
+        public static Vector3? GetCenterPointOfCar(TrainController trainController, string carId)
         {
+            if (trainController == null) return null;
+
             var carLookupField = AccessTools.Field(typeof(TrainController), "_carLookup");
-            var carLookup = (Dictionary<string, Car>)carLookupField.GetValue(__instance);
+            var carLookup = (Dictionary<string, Car>)carLookupField.GetValue(trainController);
 
             if (carLookup != null && carLookup.TryGetValue(carId, out Car car))
             {
-                __result = car.GetCenterPosition(__instance.graph);
+                return car.GetCenterPosition(trainController.graph);
             }
             else
             {
-                __result = null;
+                return null;
             }
         }
     }
 
+}
+
+
+public class StationData
+{
+    public Vector3 Pos0 { get; set; }
+    public Vector3 Pos1 { get; set; }
+    public Vector3 Center { get; set; }
+    public float Length { get; set; }
+
+    public StationData(float x0, float y0, float z0, float x1, float y1, float z1, float xc, float yc, float zc, float len)
+    {
+        Pos0 = new Vector3(x0, y0, z0);
+        Pos1 = new Vector3(x1, y1, z1);
+        Center = new Vector3(xc, yc, zc);
+        Length = len;
+    }
 }
 
 public static class StationManager
@@ -119,6 +136,25 @@ public static class StationManager
     {
         stationSelections[stop.identifier] = isSelected;
     }
+
+    public static Dictionary<string, StationData> Stations = new Dictionary<string, StationData>
+    {
+        { "sylva", new StationData(24634.5f, 620.57f, -941.23f, 24563.24f, 620.57f, -935.94f, 24598.87f, 620.57f, -938.585f, 71.45608232f) },
+        { "andrews", new StationData(-29923.78f, 538.97f, -20057.8f, -29990.74f, 538.97f, -20092.33f, -29957.26f, 538.97f, -20075.065f, 75.33898393f) },
+        { "almond", new StationData(-6340.3f, 524.97f, -1291.01f, -6316.44f, 524.97f, -1347.1f, -6328.37f, 524.97f, -1319.055f, 60.95398018f) },
+        { "hemingway", new StationData(2820.64f, 578.52f, 3079.64f, 2815.72f, 578.54f, 3055.54f, 2818.18f, 578.53f, 3067.59f, 24.59708926f) },
+        { "whittier", new StationData(12267.1f, 561.45f, 5864.33f, 12279.19f, 561.45f, 5893.68f, 12273.145f, 561.45f, 5879.005f, 31.74256763f) },
+        { "cochran", new StationData(1996.88f, 591.62f, -205.13f, 2007.29f, 591.85f, -218.98f, 2002.085f, 591.735f, -212.055f, 17.32753589f) },
+        { "alarka", new StationData(4170.52f, 644.81f, -3113.05f, 4201.17f, 645.24f, -3140.48f, 4185.845f, 645.025f, -3126.765f, 41.13407711f) },
+        { "rhodo", new StationData(-22993.12f, 653.53f, -18005.08f, -23014.11f, 653.15f, -18030.5f, -23003.615f, 653.34f, -18017.79f, 32.96818011f) },
+        { "dillsboro", new StationData(22379.87f, 603.17f, -1410.88f, 22326.76f, 603.17f, -1434.12f, 22353.315f, 603.17f, -1422.5f, 57.9721459f) },
+        { "bryson", new StationData(4530.43f, 528.97f, 5428.56f, 4473.52f, 528.97f, 5407.87f, 4501.975f, 528.97f, 5418.215f, 60.55430786f) },
+        { "ela", new StationData(9569.54f, 546.61f, 7404.1f, 9554.41f, 546.61f, 7409.92f, 9561.975f, 546.61f, 7407.01f, 16.21077728f) },
+        { "alarkajct", new StationData(1745.6f, 590.23f, 1503.32f, 1737.93f, 589.78f, 1425.91f, 1741.765f, 590.005f, 1464.615f, 77.79035609f) },
+        { "nantahala", new StationData(-15594.29f, 595.2f, -10588.8f, -15642.63f, 595.51f, -10646.29f, -15618.46f, 595.355f, -10617.545f, 75.11292698f) },
+        { "topton", new StationData(-18969.52f, 793.22f, -15217.75f, -18977.49f, 792.7f, -15231.27f, -18973.505f, 792.96f, -15224.51f, 15.70292011f) },
+        { "wilmot", new StationData(16511.31f, 569.97f, 2326.23f, 16493.52f, 569.97f, 2329.14f, 16502.415f, 569.97f, 2327.685f, 18.0264306f) }
+    };
 }
 
 
@@ -255,7 +291,7 @@ namespace RouteManagerUI
                 builder.AddField("Max Speed", control);
 
                 var stopsLookup = PassengerStop.FindAll().ToDictionary(stop => stop.identifier, stop => stop);
-                var orderedStops = new string[] { "sylva", "dillsboro", "wilmot", "whittier", "ela", "bryson", "hemingway", "alarkajct", "almond", "nantahala", "topton", "rhodo", "andrews", "cochran", "alarka" }
+                var orderedStops = new string[] { "sylva", "dillsboro", "wilmot", "whittier", "ela", "bryson", "hemingway", "alarkajct", "cochran", "alarka", "almond", "nantahala", "topton", "rhodo", "andrews" }
                                    .Select(id => stopsLookup[id])
                                    .Where(ps => !ps.ProgressionDisabled)
                                    .ToList();
@@ -283,10 +319,10 @@ namespace RouteManagerUI
                 bool anyStationSelected = StationManager.IsAnyStationSelected(orderedStops);
 
                 // If any station is selected, add a button to the UI
-                if (anyStationSelected)
+                /*if (anyStationSelected)
                 {
                     builder.AddButton("Print Car Info", () => ManagedTrains.PrintCarInfo(car));
-                }
+                }*/
 
             }
 
@@ -396,150 +432,5 @@ namespace RouteManagerUI
 }
 
 /*
-
-if (RouteManagerPlugin.IsRouteModeActive)
-            {
-                Debug.Log("It was active");
-                Debug.Log("before route menu built Checking for Autoengineer: " + mode2 + "\tIs Route Active:" + RouteManagerPlugin.IsRouteModeActive);
-                if (mode2 == AutoEngineerMode.Off && RouteManagerPlugin.IsRouteModeActive)
-                {
-                    int num = MaxSpeedMphForMode(mode2);
-                    RectTransform control = builder.AddSlider(() => persistence.Orders.MaxSpeedMph / 5, delegate
-                    {
-                        int maxSpeedMph4 = persistence.Orders.MaxSpeedMph;
-                        return maxSpeedMph4.ToString();
-                    }, delegate (float value)
-                    {
-                        int? maxSpeedMph3 = (int)(value * 5f);
-                        SetOrdersValue(null, null, maxSpeedMph3, null);
-                    }, 0f, num / 5, wholeNumbers: true);
-                    builder.AddField("Max Speed", control);
-
-                    Debug.Log("Building Route Mode Menu");
-                    // Retrieve and prepare the list of all available stations
-                    var stopsLookup = PassengerStop.FindAll().ToDictionary(stop => stop.identifier, stop => stop);
-                    var orderedStops = new string[] { "sylva", "dillsboro", "wilmot", "whittier", "ela", "bryson", "hemingway", "alarkajct", "almond", "nantahala", "topton", "rhodo", "andrews", "cochran", "alarka" }
-                                       .Select(id => stopsLookup[id])
-                                       .Where(ps => !ps.ProgressionDisabled)
-                                       .ToList();
-
-                    // Create a scrollable view to list the stations
-                    builder.VScrollView(delegate (UIPanelBuilder builder)
-                    {
-                        foreach (PassengerStop stop in orderedStops)
-                        {
-                            builder.HStack(delegate (UIPanelBuilder hstack)
-                            {
-                                // Add a checkbox for each station
-                                hstack.AddToggle(() => StationManager.IsStationSelected(stop), isOn => StationManager.SetStationSelected(stop, isOn));
-
-                                // Add a label next to the checkbox
-                                hstack.AddLabel(stop.name);
-                            });
-                        }
-                    });
-                }
-
-
-
-
-
-
-
-
-
-
-
-
-
-private void PopulatePassengerCarPanel(UIPanelBuilder builder)
-	{
-		builder.AddField("Passengers", () => _car.PassengerCountString(_cachedMarker), UIPanelBuilder.Frequency.Fast);
-		if (_stopsLookup == null || _stopsLookup.Count == 0)
-		{
-			_stopsLookup = PassengerStop.FindAll().ToDictionary((PassengerStop stop) => stop.identifier, (PassengerStop stop) => stop);
-		}
-		List<PassengerStop> orderedStops = (from id in new string[15]
-			{
-				"sylva", "dillsboro", "wilmot", "whittier", "ela", "bryson", "hemingway", "alarkajct", "almond", "nantahala",
-				"topton", "rhodo", "andrews", "cochran", "alarka"
-			}
-			select _stopsLookup[id] into ps
-			where !ps.ProgressionDisabled
-			select ps).ToList();
-		builder.VScrollView(delegate(UIPanelBuilder builder)
-		{
-			foreach (PassengerStop item in orderedStops)
-			{
-				PassengerStop stop2 = item;
-				builder.HStack(delegate(UIPanelBuilder hstack)
-				{
-					hstack.AddToggle(() => IsPassengerStopChecked(stop2), delegate(bool isOn)
-					{
-						SetPassengerStopChecked(stop2, isOn);
-					});
-					hstack.AddLocationField(FieldName, stop2, delegate
-					{
-						JumpTo(stop2);
-					});
-				});
-				string FieldName()
-				{
-					if (!_cachedMarker.HasValue)
-					{
-						return stop2.name;
-					}
-					int num = _cachedMarker.Value.CountPassengersForStop(stop2.identifier);
-					if (num != 0)
-					{
-						return stop2.name + " (" + num + ")";
-					}
-					return stop2.name;
-				}
-			}
-		});
-		builder.Spacer(6f);
-		builder.AddButtonCompact("Copy to Coupled", CopyStopsToCoupledCoaches);
-		_observers.Add(_car.KeyValueObject.Observe("ops.passengerMarker", delegate
-		{
-			if (!(_car == null))
-			{
-				_cachedMarker = _car.GetPassengerMarker();
-			}
-		}));
-	}
-
-	private void SetPassengerStopChecked(PassengerStop passengerStop, bool isOn)
-	{
-		PassengerMarker passengerMarkerOrEmpty = GetPassengerMarkerOrEmpty();
-		string identifier = passengerStop.identifier;
-		HashSet<string> destinations = passengerMarkerOrEmpty.Destinations;
-		bool flag = destinations.Contains(identifier);
-		if (isOn && !flag)
-		{
-			destinations.Add(identifier);
-		}
-		else if (!isOn && flag)
-		{
-			destinations.Remove(identifier);
-		}
-		StateManager.ApplyLocal(new SetPassengerDestinations(_car.id, destinations.ToList()));
-	}
-
-	private PassengerMarker GetPassengerMarkerOrEmpty()
-	{
-		return _car.GetPassengerMarker() ?? PassengerMarker.Empty();
-	}
-
-	private bool IsPassengerStopChecked(PassengerStop passengerStop)
-	{
-		if (_cachedMarker.HasValue)
-		{
-			return _cachedMarker.Value.Destinations.Contains(passengerStop.identifier);
-		}
-		return false;
-	}
-
-
 
 */
